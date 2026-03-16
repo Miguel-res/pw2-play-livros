9a5f12ac2b01930c95c4a7569932c3175f2a0c80
ma21 <resciamiguel@gmail.com>
2026-03-16 08:05:10 -0300

    criação da classe livro

---
+++ src/Livro.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+public class Livro {
+    String titulo;
+    String autor;
+    String narrado;
+    String editora;
+    LocalTime duracao;
+    String versao;
+    double preco;
+    String resumo;
+}
