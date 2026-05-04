66d5ceb4c20a728b8d68bcb2fe69e5ef9b4fa027
MiguelMatheus <resciamiguel@gmail.com>
2026-05-04 08:18:02 -0300

    Criação do método obterPorExtenso em Avaliação

--- src/br/com/etechoracio/playlivros/model/Avaliacao.java
+++ src/br/com/etechoracio/playlivros/model/Avaliacao.java
@@ -1,6 +1,7 @@
 package br.com.etechoracio.playlivros.model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Avaliacao {
         public int estrelas;
@@ -10,10 +11,16 @@ public class Avaliacao {
         public Usuario usuario;
         public boolean aprovado;
 
-        public void exibir(){
+        public void exibir() {
                 System.out.println("Usuario" + usuario);
                 System.out.println(estrelas + " Estrelas " + titulo);
-                System.out.println("Data" + data);
+                System.out.println("Avaliado em " + obterDataPorExtenso());
                 System.out.println("Comentario" + comentario);
+        }
+
+        public String obterDataPorExtenso() {
+                DateTimeFormatter formatador = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyyy");
+                String resultado = formatador.format(data);
+                return resultado;
         }
 }
